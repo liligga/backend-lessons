fbd8ff89da4c8554cad13f51973416fe4bec8276
Igor <igglike@gmail.com>
2024-04-08 18:08:06 +0600

    Added new lessons

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Igor`s Site';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Zzdev сайт';
+export const SITE_DESCRIPTION = 'Лекции и статьи по Python, Javascript, React и т.д.';
 export const BASE_URL = '/bakend-lessons'
---
+++ src/content/lesson/python_bot_configuration.md
@@ -0,0 +1,7 @@
+---
+title: 'Настройка бота в Aiogram'
+description: 'Настройка бота в Aiogram'
+draft: true
+slug: '45307f6e-daf5-4054-ad37-f09e6105070f'
+order: 2.1
+---
