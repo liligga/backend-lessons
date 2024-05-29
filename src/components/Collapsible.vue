6e2c958eb789df837181e19ab54359cca379b927
Igor <igglike@gmail.com>
2024-05-29 14:49:12 +0600

    Добавил вопросы по redux

---
+++ src/components/Collapsible.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="collapsible">
+    <!-- <h3><slot name="title"></slot></h3> -->
+    <!-- <p><slot name="content"></slot></p> -->
+    <slot name="title">Fallback</slot>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+.collapsible  {
+  transition: all 0.3s;
+}
+.collapsible p {
+  display: none;
+}
+</style>
